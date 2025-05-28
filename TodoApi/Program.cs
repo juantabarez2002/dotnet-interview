3d0a19a206ccb68dc0630e3fecce20bd39d74f7d
Juan Tabarez <JT270871@fi365.ort.edu.uy>
2025-05-28 17:27:06 -0300

    feat: Add Docker

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -13,6 +13,12 @@ builder.Logging.AddConsole();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<TodoContext>();
+    db.Database.Migrate();
+}
+
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
